53607a10efbe3ccb91f025bff148b8f65944232c
ValentinaZussino <vzussino@protonmail.com>
2022-12-19 15:34:34 +0100

    agg campi tabella + creaz seeder

--- database/migrations/2022_12_19_135548_create_trains_table.php
+++ database/migrations/2022_12_19_135548_create_trains_table.php
@@ -16,6 +16,16 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda', 150);
+            $table->string('stazione_di_partenza', 200);
+            $table->string('stazione_di_arrivo', 200);
+            $table->time('ora_di_arrivo', $precision = 2);
+            $table->time('ora_di_partenza', $precision = 2);
+            $table->string('codice_treno');
+            $table->integer('n_carrozza') -> nullable();
+            $table->boolean('in_orario')-> nullable();
+            $table->boolean('cancellato')-> nullable();
+
         });
     }
 
